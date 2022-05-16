f071097c30bfbbb4c80f11dda51817233ec5d28d
recvmsgs <105562133+recvmsgs@users.noreply.github.com>
2022-05-16 14:41:02 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,34 +1,31 @@
-#!/bin/bash
+#!/bin/bash -xv
 
-source ./config.sh
-
-cd "$WORKDIR"
-echo "$PWD"
 {
 [[ -z "${SKIT_ALERT_EMAIL}" ]] && source ./config.sh
+
+cd "$WORKDIR"
+echo "$PWD" "$SKIT_ALERT_EMAIL"
 
 URL='https://status.digitalocean.com/api/v2/summary.json'
 
 [[ "$1" = 'test' ]] && URL='https://status.digitalocean.com/api/v2/scheduled-maintenances.json'
 
-RESPONSE="$(curl -s $URL |python3 -m json.tool)"
+RESPONSE="$(curl -s $URL)"
 
 MSGFILE="/tmp/do-alert----4f28affe-0f5b-4003-99c9.json"
 
 { echo "$RESPONSE" \
-    | egrep -qo '^            "status": "operational"|^    "incidents": \[\],|    "scheduled_maintenances": \[\],';
+    | fgrep '"status":"operational",' | fgrep '"incidents":[],' | fgrep '"scheduled_maintenances":[],';
 }   \
 &&  {
     echo OOOOOOOOOOOK ;
     } \
 ||  {
     echo BAAAAAAAAAAD;
-    echo "$RESPONSE" >| "$MSGFILE";
+    echo "$RESPONSE" | python3 -m json.tool >| "$MSGFILE";
     sendemail -f "$MAIL_FROM"  -t "$SKIT_ALERT_EMAIL" -u 'DigitalOcean ALERT' \
               -m "ALERT, see https://status.digitalocean.com/ or the  attachment" -a "$MSGFILE"  \
               -s "$SMTP_ADDR":"$SMTP_PORT" -o tls=yes -xu "$MAIL_FROM" -xp "$MAIL_PASS";
     exit 1;
     }
-} #>| ./log 2>| ./log2
-
-echo "$SKIT_ALERT_EMAIL"
+} >| ./log 2>> ./log
