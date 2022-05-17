65d83fff078899112de8ef9d4276bad099f5142c
recvmsgs <105562133+recvmsgs@users.noreply.github.com>
2022-05-17 12:45:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,26 @@ WORKDIR="$PWD"
 sed -i '/^WORKDIR=/d' ./config.sh
 echo WORKDIR="$WORKDIR" >> ./config.sh
 
-echo "$@" | tr ' ' '\n' >> ./config.sh
+eval "$@"
+echo "$@"
+
+[[ ! -z "$SKIT_ALERT_EMAIL" ]] \
+   && echo SKIT_EMAIL \
+   && sed -i '/^SKIT_ALERT_EMAIL=/d' ./config.sh \
+   && echo 'SKIT_ALERT_EMAIL='"$SKIT_ALERT_EMAIL" >> ./config.sh
+[[ ! -z "$MAIL_FROM" ]] \
+   && echo MAIL_FROM \
+   && sed -i '/^MAIL_FROM=/d' ./config.sh \
+   && echo 'MAIL_FROM='"$MAIL_FROM" >> ./config.sh
+[[ ! -z "$MAIL_ADDR" ]] \
+  && echo MAIL_ADDR \
+  && sed -i '/^MAIL_ADDR=/d' ./config.sh \
+  && echo 'MAIL_ADDR='"$MAIL_ADDR" >> ./config.sh
+[[ ! -z "$MAIL_PORT" ]] \
+  && echo MAIL_PORT \
+  && sed -i '/^MAIL_PORT=/d' ./config.sh \
+  && echo 'MAIL_PORT='"$MAIL_PORT" >> ./config.sh
+
 
 sudo apt -y install libio-socket-ssl-perl libnet-ssleay-perl sendemail
 
